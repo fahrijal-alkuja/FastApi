c077aedd57ba332bee81415389395d4e730a2e65
fahrijal-alkuja <alkuja@unikarta.ac.id>
2023-04-16 01:02:29 +0800

    fix

--- config/auth_bearer.py
+++ config/auth_bearer.py
@@ -15,27 +15,6 @@ def get_user(db, email: str):
     return db.query(Users).filter(Users.email == email).first()
 
 
-# async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         email: str = payload.get("sub")
-
-#         if email is None:
-#             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-#                                 detail="Invalid authentication credentials")
-
-#         if not check_token_expaid(token):
-#             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-#                                 detail="Token has expired")
-#     except JWTError:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-#                             detail="Invalid authentication credentials")
-#     user = get_user(db, email)
-#     if user is None:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-#                             detail="Invalid authentication credentials")
-#     return user
-
 async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
